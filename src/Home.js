f31e993161da46fab72f7af8a3940a79692e08e0
Valentin Barit <valentin.barit@gmail.com>
2022-02-17 10:56:02 +0100

    feat: 🎸 sync search input and URL

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,19 @@
 import { useState } from "react";
+import { useSearchParams } from "react-router-dom";
+
 import Input from "./Input";
 import VerticalList from "./VerticalList";
-
 import data from "./data.json";
 import useStyles from "./Home.style";
 
 function Home() {
+  const [searchParams, setSearchParams] = useSearchParams();
   const classes = useStyles();
-  const [value, setValue] = useState("");
-  const onChange = (event) => setValue(event.target.value);
+  const [value, setValue] = useState(searchParams.get("q"));
+  const onChange = (event) => {
+    setValue(event.target.value);
+    setSearchParams(event.target.value ? { q: event.target.value } : {});
+  };
   const movies = data.movies.filter((movie) =>
     movie.title.match(new RegExp(value, "i"))
   );
